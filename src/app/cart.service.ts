e00b90a8c4ee17f9cbd0defb9daf66372a6f9bc2
y-ebneddin <y_ebneddin@yahoo.com>
2023-04-04 16:57:53 +0200

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 /* . . . */
@@ -5,8 +6,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
+  
   items: Product[] = [];
-/* . . . */
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
 
   addToCart(product: Product) {
     this.items.push(product);
